924dde44b9910e2aef80b3fb318e8941a4f3481c
Oduwole Faruq Adetola <oduwoleadetohlar@gmail.com>
2024-09-11 16:37:30 +0100

    fix: code for button component out and all <img> tags changed

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -1,26 +1,29 @@
-import { Footer } from "@/components/common/Footer";
-import { Header } from "@/components/common/Header";
-import HeroSection from "@/components/specific/solutions/HeroSection";
+// import { Footer } from '@/components/common/Footer';
+// import { Header } from '@/components/common/Header';
+import HeroSection from '@/components/specific/solutions/HeroSection';
+import Image from 'next/image';
 
 const Solutions = () => {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <HeroSection />
-      <section className="flex flex-col items-center bg-neutral space-y-8 py-8">
+      <section className="flex flex-col items-center bg-neutral">
         <div className="flex flex-col lg:flex-row items-center justify-center space-y-6 lg:space-y-0 lg:space-x-6">
           <div className="flex justify-center bg-[#F9F9D1] w-[280px] h-[280px] lg:w-[380px] lg:h-[350px] rounded-tl-[20px] rounded-tr-[80px] rounded-br-[20px] rounded-bl-[80px]">
             <div className="w-full mt-[10px] ml-[20px] lg:ml-[30px] lg:mt-[20px] overflow-hidden">
-              <img
+              <Image
                 src="/images/grommet-icons_organization.svg"
                 alt="grommet-icon"
                 className="w-[15px] h-[15px] lg:w-[30px] lg:h-[30px]"
+                width={30}
+                height={30}
               />
               <p className="text-secondary text-xs lg:text-[16px] font-semibold mt-[10px] lg:mt-[15px]">
                 SmartClinic for Organizations
               </p>
               <p className="text-secondary text-[12px] lg:text-sm  font-normal mt-[5px] lg:mt-[15px] truncate break-words">
-                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{" "}
+                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{' '}
                 nascetur id scelerisque facilisis morbi donec <br /> malesuada
                 sed. Diam bibendum feugiat eget neque <br /> risus. Odio
                 pulvinar enim ultricies proin. Sed sem <br /> egestas sit nunc
@@ -28,42 +31,48 @@ const Solutions = () => {
                 Quis massa eget <br /> vestibulum aliquam. Diam facilisis est
                 amet egestas <br /> etiam volutpat feugiat. Ultrices lacus sed
                 at at sit ut <br /> risus. Commodo nibh elit eu amet. Nunc
-                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{" "}
+                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{' '}
                 <br /> imperdiet massa ipsum. Massa.
               </p>
             </div>
           </div>
 
           <div className="w-full lg:w-1/2 flex justify-center">
-            <img
+            <Image
               src="/images/doctor-laptop.svg"
               alt="doctor-laptop"
               className="w-[280px] h-[280px] lg:w-[380px] lg:h-[380px]"
+              width={380}
+              height={380}
             />
           </div>
         </div>
 
         <div className="flex flex-col lg:flex-row items-center justify-center space-y-6 lg:space-y-0 lg:space-x-6">
           <div className="w-full lg:w-1/2 flex justify-center">
-            <img
+            <Image
               src="/images/doctor-facetime.svg"
               alt="doctor-facetime"
               className="w-[280px] h-[280px] lg:w-[380px] lg:h-[380px]"
+              width={380}
+              height={380}
             />
           </div>
 
           <div className="flex justify-center bg-[#F9F9D1] w-[280px] h-[280px] lg:w-[380px] lg:h-[350px] rounded-tl-[20px] rounded-tr-[80px] rounded-br-[20px] rounded-bl-[80px]">
             <div className="w-full mt-[10px] ml-[20px] lg:ml-[30px] lg:mt-[20px] overflow-hidden">
-              <img
+              <Image
                 src="/images/patient-icon.svg"
                 alt="patient-icon"
                 className="w-[15px] h-[15px] lg:w-[30px] lg:h-[30px]"
+                width={30}
+                height={30}
               />
               <p className="text-secondary text-xs lg:text-[16px] font-semibold mt-[10px] lg:mt-[15px]">
                 SmartClinic for Patients
               </p>
               <p className="text-secondary text-[12px] lg:text-sm  font-normal mt-[5px] lg:mt-[15px] truncate break-words">
-                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{" "}
+                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{' '}
                 nascetur id scelerisque facilisis morbi donec <br /> malesuada
                 sed. Diam bibendum feugiat eget neque <br /> risus. Odio
                 pulvinar enim ultricies proin. Sed sem <br /> egestas sit nunc
@@ -71,7 +80,7 @@ const Solutions = () => {
                 Quis massa eget <br /> vestibulum aliquam. Diam facilisis est
                 amet egestas <br /> etiam volutpat feugiat. Ultrices lacus sed
                 at at sit ut <br /> risus. Commodo nibh elit eu amet. Nunc
-                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{" "}
+                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{' '}
                 <br /> imperdiet massa ipsum. Massa.
               </p>
             </div>
@@ -81,16 +90,18 @@ const Solutions = () => {
         <div className="flex flex-col lg:flex-row items-center justify-center space-y-6 lg:space-y-0 lg:space-x-6">
           <div className="flex justify-center bg-[#F9F9D1] w-[280px] h-[280px] lg:w-[380px] lg:h-[350px] rounded-tl-[20px] rounded-tr-[80px] rounded-br-[20px] rounded-bl-[80px]">
             <div className="w-full mt-[10px] ml-[20px] lg:ml-[30px] lg:mt-[20px] overflow-hidden">
-              <img
+              <Image
                 src="/images/patient-icon.svg"
                 alt="patient-icon"
                 className="w-[15px] h-[15px] lg:w-[30px] lg:h-[30px]"
+                width={30}
+                height={30}
               />
               <p className="text-secondary text-xs lg:text-[16px] font-semibold mt-[10px] lg:mt-[15px]">
                 SmartClinic for Providers
               </p>
               <p className="text-secondary text-[12px] lg:text-sm  font-normal mt-[5px] lg:mt-[15px] truncate break-words">
-                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{" "}
+                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{' '}
                 nascetur id scelerisque facilisis morbi donec <br /> malesuada
                 sed. Diam bibendum feugiat eget neque <br /> risus. Odio
                 pulvinar enim ultricies proin. Sed sem <br /> egestas sit nunc
@@ -98,42 +109,48 @@ const Solutions = () => {
                 Quis massa eget <br /> vestibulum aliquam. Diam facilisis est
                 amet egestas <br /> etiam volutpat feugiat. Ultrices lacus sed
                 at at sit ut <br /> risus. Commodo nibh elit eu amet. Nunc
-                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{" "}
+                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{' '}
                 <br /> imperdiet massa ipsum. Massa.
               </p>
             </div>
           </div>
 
           <div className="w-full lg:w-1/2 flex justify-center">
-            <img
+            <Image
               src="/images/doctor-lab.svg"
               alt="doctor-lab"
               className="w-[280px] h-[280px] lg:w-[380px] lg:h-[380px]"
+              width={380}
+              height={380}
             />
           </div>
         </div>
 
         <div className="flex flex-col lg:flex-row items-center justify-center space-y-6 lg:space-y-0 lg:space-x-6">
           <div className="w-full lg:w-1/2 flex justify-center">
-            <img
+            <Image
               src="/images/man-meeting.svg"
               alt="man-meeting"
               className="w-[280px] h-[280px] lg:w-[380px] lg:h-[380px]"
+              width={380}
+              height={380}
             />
           </div>
 
           <div className="flex justify-center bg-[#F9F9D1] w-[280px] h-[280px] lg:w-[380px] lg:h-[350px] rounded-tl-[20px] rounded-tr-[80px] rounded-br-[20px] rounded-bl-[80px]">
             <div className="w-full mt-[10px] ml-[20px] lg:ml-[30px] lg:mt-[20px] overflow-hidden">
-              <img
+              <Image
                 src="/images/file.svg"
                 alt="file-icon"
                 className="w-[15px] h-[15px] lg:w-[30px] lg:h-[30px]"
+                width={30}
+                height={30}
               />
               <p className="text-secondary text-xs lg:text-[16px] font-semibold mt-[10px] lg:mt-[15px]">
                 SmartClinic for Planners
               </p>
               <p className="text-secondary text-[12px] lg:text-sm  font-normal mt-[5px] lg:mt-[15px] truncate break-words">
-                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{" "}
+                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{' '}
                 nascetur id scelerisque facilisis morbi donec <br /> malesuada
                 sed. Diam bibendum feugiat eget neque <br /> risus. Odio
                 pulvinar enim ultricies proin. Sed sem <br /> egestas sit nunc
@@ -141,7 +158,7 @@ const Solutions = () => {
                 Quis massa eget <br /> vestibulum aliquam. Diam facilisis est
                 amet egestas <br /> etiam volutpat feugiat. Ultrices lacus sed
                 at at sit ut <br /> risus. Commodo nibh elit eu amet. Nunc
-                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{" "}
+                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{' '}
                 <br /> imperdiet massa ipsum. Massa.
               </p>
             </div>
@@ -151,16 +168,18 @@ const Solutions = () => {
         <div className="flex flex-col lg:flex-row items-center justify-center space-y-6 lg:space-y-0 lg:space-x-6">
           <div className="flex justify-center bg-[#F9F9D1] w-[280px] h-[280px] lg:w-[380px] lg:h-[350px] rounded-tl-[20px] rounded-tr-[80px] rounded-br-[20px] rounded-bl-[80px]">
             <div className="w-full mt-[10px] ml-[20px] lg:ml-[30px] lg:mt-[20px] overflow-hidden">
-              <img
+              <Image
                 src="/images/desktop-icon.svg"
                 alt="desktop-icon"
                 className="w-[15px] h-[15px] lg:w-[30px] lg:h-[30px]"
+                width={30}
+                height={30}
               />
               <p className="text-secondary text-xs lg:text-[16px] font-semibold mt-[10px] lg:mt-[15px]">
                 SmartClinic for Developers
               </p>
               <p className="text-secondary text-[12px] lg:text-sm  font-normal mt-[5px] lg:mt-[15px] truncate break-words">
-                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{" "}
+                Lorem ipsum dolor sit amet consectetur. Eleifend leo <br />{' '}
                 nascetur id scelerisque facilisis morbi donec <br /> malesuada
                 sed. Diam bibendum feugiat eget neque <br /> risus. Odio
                 pulvinar enim ultricies proin. Sed sem <br /> egestas sit nunc
@@ -168,22 +187,24 @@ const Solutions = () => {
                 Quis massa eget <br /> vestibulum aliquam. Diam facilisis est
                 amet egestas <br /> etiam volutpat feugiat. Ultrices lacus sed
                 at at sit ut <br /> risus. Commodo nibh elit eu amet. Nunc
-                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{" "}
+                scelerisque <br /> commodo vulputate ac. Eu dictum auctor at{' '}
                 <br /> imperdiet massa ipsum. Massa.
               </p>
             </div>
           </div>
 
           <div className="w-full lg:w-1/2 flex justify-center">
-            <img
+            <Image
               src="/images/developer.svg"
               alt="developer-img"
               className="w-[280px] h-[280px] lg:w-[380px] lg:h-[380px]"
+              width={380}
+              height={380}
             />
           </div>
         </div>
       </section>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
--- src/components/specific/solutions/HeroSection.tsx
+++ src/components/specific/solutions/HeroSection.tsx
@@ -1,16 +1,20 @@
-import Button from "@/components/common/Button";
+import Image from 'next/image';
+
+// import Button from "@/components/common/Button";
 const HeroSection = () => {
   return (
     <section>
       <div className="relative w-full h-[800px] flex">
-        <img
+        <Image
           src="/images/Solutions_BG.png"
-          alt="solutions-bg"
           className="w-full h-full object-cover"
+          width={100}
+          height={100}
+          alt="solutions-bg"
         />
         <div className="absolute inset-0 flex flex-col space-y-4 p-4 top-60 left-10 lg:top-60 lg:left-20">
           <p className="text-[28px] font-bold leading-8">
-            We exist for different types of <br />{" "}
+            We exist for different types of <br />{' '}
             <span className="text-primary">Users</span>
           </p>
           <p className="text-[16px] font-normal text-primedText mt-[20px]">
@@ -18,7 +22,7 @@ const HeroSection = () => {
             day-to-day dealings associated with the <br /> health sector
           </p>
           <div className="mt-[20px]">
-            <Button label="More About Us" variant="primary" />
+            {/* <Button label="More About Us" variant="primary" /> */}
           </div>
         </div>
       </div>
