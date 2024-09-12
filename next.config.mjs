795df3ea84a46ac472c4e79edec569bf32d66d5e
Usman Ogunsola <ogunsoladebayo@gmail.com>
2024-09-13 00:30:09 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-        unoptimized: true, // Disable image optimization for static exports
-    },
     transpilePackages: ['react-daisyui'],
     reactStrictMode: true,
 };
